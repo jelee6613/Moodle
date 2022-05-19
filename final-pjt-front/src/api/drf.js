b5b5eab9e091e58f241a32db1b9bb0728c192b17
joorii <joori0709@gmail.com>
2022-05-20 01:20:26 +0900

    마주리 2일차, 3일차

---
+++ final-pjt-front/src/api/drf.js
@@ -0,0 +1,30 @@
+const HOST = 'http://localhost:8000/'
+
+const ACCOUNTS = 'accounts/'
+const MOVIES = 'movies/'
+const COMMUNITY = 'community/'
+
+export default {
+    accounts: {
+        login: () => HOST + ACCOUNTS + 'login/',
+        logout: () => HOST + ACCOUNTS + 'logout/',
+        signup: () => HOST + ACCOUNTS + 'signup/',
+        profile: username => HOST + ACCOUNTS + 'profile/' + username,
+        follow: username => HOST + ACCOUNTS + 'profile/' + username + '/follow/',
+        watchedMovie: username => HOST + ACCOUNTS + username + '/watched/'
+    },
+    movies: {
+        movies: () => HOST,
+        movie: movieId => HOST + MOVIES + movieId,
+        movieRecommendation: () => HOST + MOVIES + 'recommendations/',
+        watchedMovie: (username, moviePk) => HOST + MOVIES + username + '/watched/' + moviePk,
+        movieRate: (username, moviePk) => HOST + MOVIES + username + '/watched/' + moviePk + '/rate/'
+    },
+    articles: {
+        articles: () => HOST + COMMUNITY,
+        article: articlePk => HOST + COMMUNITY + articlePk,
+        comments: articlePk => HOST + COMMUNITY + articlePk + '/comment/',
+        comment: (articlePk, commentPk) => HOST + COMMUNITY + `${articlePk}/` + 'comment/' + commentPk,
+        likeArticle: articlePk => HOST + COMMUNITY + articlePk + '/like/'
+    }
+}
--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -1,17 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import accounts from './modules/accounts'
+import movies from './modules/movies'
+import articles from './modules/articles'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    accounts,
+    movies,
+    articles
   }
 })
