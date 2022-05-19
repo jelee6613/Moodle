b1c3e7c2fc133657183f46cbc90b0c680f554e4b
joorii <joori0709@gmail.com>
2022-05-19 16:13:30 +0900

    accounts, movies component skeleton code

--- final-pjt-front/src/views/MovieListView.vue
+++ final-pjt-front/src/views/MovieListView.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-
+    <movie-list-item></movie-list-item>
   </div>
 </template>
 
 <script>
+import MovieListItem from '@/components/MovieListItem.vue'
+
 export default {
-    name: 'MovieListView',
+  name: 'MovieListView',
+  components: { MovieListItem }
 }
 </script>
 
--- final-pjt-front/src/views/MovieRecommendationView.vue
+++ final-pjt-front/src/views/MovieRecommendationView.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-
+    <router-link :to="{ name: 'RecommendedList'}">Recommended List</router-link>
+    <quiz-form></quiz-form>
   </div>
 </template>
 
 <script>
+import QuizForm from '@/components/QuizForm.vue'
+
 export default {
-    name: 'MovieRecommendationView',
+  name: 'MovieRecommendationView',
+  components: { QuizForm }
 }
 </script>
 
