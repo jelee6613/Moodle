d47ac307ecc47253db8671def7ed64fbee4a6d01
jelee6613 <jelee6613@gmail.com>
2022-05-19 16:13:35 +0900

    community component skeleton code

--- final-pjt-front/src/views/ArticleCreateView.vue
+++ final-pjt-front/src/views/ArticleCreateView.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-
+    <article-form></article-form>
   </div>
 </template>
 
 <script>
+import ArticleForm from '../components/ArticleForm.vue'
+
+
 export default {
     name: 'ArticleCreateView',
+    components: {
+      ArticleForm,
+    },
 }
 </script>
 
