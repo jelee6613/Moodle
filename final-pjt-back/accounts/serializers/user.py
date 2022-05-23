3c18145cb0248898189d70fb1a0eb129a92c8277
jelee6613 <jelee6613@gmail.com>
2022-05-23 13:13:35 +0900

    add accounts

---
+++ final-pjt-back/accounts/serializers/user.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from movies.models import Movie
+from movies.serializers.movie import MovieDetailSerializer, MovieListSerializer
+from articles.serializers.article import ArticleSerializer
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class UserFollowSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = User
+            fields = ('pk', 'username')
+
+    movies = MovieDetailSerializer(many=True)
+    article_set = ArticleSerializer(many=True)
+    followings = UserFollowSerializer(many=True)
+    followers = UserFollowSerializer(many=True)
+
+    class Meta:
+        model = User
+        fields = ('pk', 'username', 'movies', 'followings', 'followers', 'article_set',)
+
+class WatchedMoviesSerializer(serializers.ModelSerializer):
+    
+    movie_id = MovieListSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        fields = ('__all__')
